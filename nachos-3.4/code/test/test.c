04121eaf218e9845efc200b6b29c0e6abf5e01eb
Lê Minh Quân <78531303+marucube35@users.noreply.github.com>
2022-11-25 17:54:42 +0700

    feat: add SC_Create

---
+++ nachos-3.4/code/test/test.c
@@ -0,0 +1,20 @@
+#include "copyright.h"
+#include "syscall.h"
+#define maxlen 32
+int main()
+{
+    int len;
+    char filename[maxlen + 1];
+    /*Create a file*/
+    if (Create("text.txt") == -1)
+    {
+        // xuất thông báo lỗi tạo tập tin
+    }
+    else
+    {
+        // xuất thông báo tạo tập tin thành công
+    }
+    Halt();
+
+    return 0;
+}
--- nachos-3.4/code/userprog/exception.cc
+++ nachos-3.4/code/userprog/exception.cc
@@ -1,4 +1,4 @@
-// exception.cc 
+// exception.cc
 //	Entry point into the Nachos kernel from user programs.
 //	There are two kinds of things that can cause control to
 //	transfer back to here from user code:
@@ -9,7 +9,7 @@
 //
 //	exceptions -- The user code does something that the CPU can't handle.
 //	For instance, accessing memory that doesn't exist, arithmetic errors,
-//	etc.  
+//	etc.
 //
 //	Interrupts (which can also cause control to transfer from user
 //	code into the Nachos kernel) are handled elsewhere.
@@ -18,12 +18,12 @@
 // Everything else core dumps.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
-#include "system.h"
 #include "syscall.h"
+#include "system.h"
 
 //----------------------------------------------------------------------
 // ExceptionHandler
@@ -39,25 +39,123 @@
 //		arg3 -- r6
 //		arg4 -- r7
 //
-//	The result of the system call, if any, must be put back into r2. 
+//	The result of the system call, if any, must be put back into r2.
 //
 // And don't forget to increment the pc before returning. (Or else you'll
 // loop making the same system call forever!
 //
-//	"which" is the kind of exception.  The list of possible exceptions 
+//	"which" is the kind of exception.  The list of possible exceptions
 //	are in machine.h.
 //----------------------------------------------------------------------
 
-void
-ExceptionHandler(ExceptionType which)
+char *UserToSystem(int virtAddr, int limit)
+{
+    int i; // index
+    int oneChar;
+    char *kernelBuf = NULL;
+    kernelBuf = new char[limit + 1]; // need for terminal string
+
+    if (kernelBuf == NULL)
+        return kernelBuf;
+    memset(kernelBuf, 0, limit + 1);
+
+    for (i = 0; i < limit; i++)
+    {
+        machine->ReadMem(virtAddr + i, 1, &oneChar);
+        kernelBuf[i] = (char)oneChar;
+
+        if (oneChar == 0)
+            break;
+    }
+
+    return kernelBuf;
+}
+
+int SystemToUser(int virtAddr, int len, char *buffer)
+{
+    if (len < 0)
+        return -1;
+    if (len == 0)
+        return len;
+
+    int i = 0;
+    int oneChar = 0;
+
+    do
+    {
+        oneChar = (int)buffer[i];
+        machine->WriteMem(virtAddr + i, 1, oneChar);
+        i++;
+    } while (i < len && oneChar != 0);
+
+    return i;
+}
+
+void ExceptionHandler(ExceptionType which)
 {
     int type = machine->ReadRegister(2);
 
-    if ((which == SyscallException) && (type == SC_Halt)) {
-	DEBUG('a', "Shutdown, initiated by user program.\n");
-   	interrupt->Halt();
-    } else {
-	printf("Unexpected user mode exception %d %d\n", which, type);
-	ASSERT(FALSE);
+    switch (which)
+    {
+    case NoException:
+        return;
+    case SyscallException:
+        switch (type)
+        {
+        case SC_Halt:
+            DEBUG('a', "Shutdown, initiated by user program.\n");
+            printf("Shutdown, initiated by user program.\n");
+
+            interrupt->Halt();
+            break;
+        case SC_Create:
+        {
+            int virtAddr;
+            char *filename;
+
+            DEBUG('a', "SC_Create call ...\n");
+            DEBUG('a', "Reading virtual address of filename\n");
+
+            // Lấy tham số tên tập tin từ thanh ghi r4
+            virtAddr = machine->ReadRegister(4);
+            DEBUG('a', "Reading filename.\n");
+
+            // Lấy dữ liệu từ vùng nhớ thông qua địa chỉ ảo
+            int MaxFileLength = 32;
+            filename = UserToSystem(virtAddr, MaxFileLength + 1);
+
+            // Trường hợp không đủ vùng nhớ để lưu tên file
+            if (filename == NULL)
+            {
+                DEBUG('a', "Not enough memory in system.\n");
+                printf("Not enough memory in system.\n");
+
+                machine->WriteRegister(2, -1); // trả về giá trị -1 cho thanh ghi r2 (lỗi)
+                delete filename;
+                return;
+            }
+
+            DEBUG('a', "Finish reading filename.\n");
+            printf("\nCreate file: %s\n", filename);
+
+            // Trường hợp không thể tạo file
+            if (!fileSystem->Create(filename, 0))
+            {
+                printf("Error create file '%s'\n", filename);
+
+                machine->WriteRegister(2, -1); // trả về giá trị -1 cho thanh ghi r2 (lỗi)
+                delete filename;
+                return;
+            }
+
+            machine->WriteRegister(2, 0); // trả về giá trị 0 cho thanh ghi r2 (thành công)
+            delete filename;
+            break;
+        }
+        default:
+            printf("\n Unexpected user mode exception (%d %d)", which,
+                   type);
+            interrupt->Halt();
+        }
     }
 }
